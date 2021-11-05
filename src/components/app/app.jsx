88675c327ce15145b75dcecdb68c6fa0a3a67ecf
yaroslav grigorev <yaroslav.purple@gmail.com>
2021-11-05 22:17:35 +0300

    add routes

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
+import {Switch, Route, BrowserRouter} from 'react-router-dom';
 import Converter from '../converter/converter';
 import CurrentRate from '../current-rate/current-rate';
+import NotFoundPage from '../not-found/not-found';
 
 const App = () => {
   return(
     <React.Fragment>
-      <Converter/>
-      <CurrentRate/>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Converter/>
+          </Route>
+          <Route exact path="/current-rate">
+            <CurrentRate/>
+          </Route>
+          <Route>
+            <NotFoundPage/>
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </React.Fragment>
   );
 };
---
+++ src/components/not-found/not-found.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFoundPage = () => {
+  return (
+    <React.Fragment>
+      <h1>Page Not Found 404</h1>
+      <button>To main page</button>
+    </React.Fragment>
+  );
+};
+
+export default NotFoundPage;
